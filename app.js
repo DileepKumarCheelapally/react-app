af50e9346c12657d505b8a6aa86fcf9235a3ebfe
DileepKumarCheelapally <ch.dillu@gmail.com>
2018-06-24 12:29:27 -0500

    Changes to deploy in Heroku

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const logger = require('tracer').colorConsole();
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,15 +11,19 @@ const port = process.env.PORT || 3000;
 // init database
 const db = require('./utils/database');
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(function(req, res, next) {
-//   logger.info(req.body);
-//   next();
-// });
 
 var appRoutes = require('./routes/router')(app);
 
